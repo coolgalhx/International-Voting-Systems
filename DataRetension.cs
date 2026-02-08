60931ac41213fa283bbf0c5cb05532926168657e
hxnxwx <152034308+hxnxwx@users.noreply.github.com>
2026-02-08 19:53:27 +0000

    implemented data retension with a singleton design pattern

--- DataRetension.cs
+++ DataRetension.cs
@@ -11,25 +11,45 @@ using Microsoft.EntityFrameworkCore;
 
 namespace International_Voting_Systems
 {
-    internal class DataRetension 
+
+
+    public sealed class DataRetension
     {
+        // The DataRetension's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private DataRetension() { }
 
+        // The DataRetension's instance is stored in a static field. There there are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static DataRetension _instance;
 
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static DataRetension GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DataRetension();
+            }
+            return _instance;
+        }
 
-
-
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
         public void DeleteVoterTable()
         {
             using var context = new MainDatabaseContext();
 
             context.Database.ExecuteSqlRaw("DELETE FROM Voters");
-
-
         }
-        
+
 
     }
-    
-        
+
+
 }
 
